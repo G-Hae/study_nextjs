030ad5380871beeab89ac0dc9cbfdd2febdccddc
G-Hae <sifialee@gmail.com>
2025-02-02 17:17:43 +0900

    fix: typeError

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -3,17 +3,19 @@ import MovieInfo, { getMovieDetail } from '../../../components/movie-info';
 import MovieVideos from '../../../components/movie-videos';
 
 interface IParams {
-    params: { id: string };
+    params: Promise<{ id: string }>;
 }
 
-export async function generateMetadata({ params: { id } }: IParams) {
+export async function generateMetadata({ params }: IParams) {
+    const { id } = await params;
     const movie = await getMovieDetail(id);
     return {
-        title: movie.title,
+        title: movie,
     };
 }
 
-export default async function MovieDetail({ params: { id } }: IParams) {
+export default async function MovieDetail({ params }: IParams) {
+    const { id } = await params;
     return (
         <div>
             <Suspense fallback={<h1>Loading Movie Info</h1>}>
